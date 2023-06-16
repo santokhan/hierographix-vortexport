c818a6c7d1f8c0cee1614f637eb9c622abf11d7d
Santo Khan <santokhan1999@gmail.com>
2023-06-17 01:45:28 +0600

    update hero section

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -2,23 +2,28 @@ import Image from "next/image";
 import HeroImg from "../../../app/assets/images/hero-illustration.png";
 import styles from "./Hero.module.css";
 import SubscribeForm from "./SubscribeForm";
+import { Manrope } from "next/font/google";
+import Text from "@/components/shared/text/Text";
+
+const manrope = Manrope({ subsets: ["latin"] });
 
 function Hero() {
   return (
-    <div className="relative overflow-hidden px-4">
+    <div className="relative overflow-hidden">
       <Effect />
-      <div className="max-w-screen-xl mx-auto pb-20">
-        <div className="max-w-screen-lg mx-auto flex flex-col items-center space-y-8 py-16 lg:py-20">
-          <h2 className="text-4xl lg:text-5xl font-bold text-start lg:text-center leading-[1.2]">
+      <div className="max-w-screen-xl mx-auto px-4 pb-20 relative">
+        <div className="max-w-[800px] mx-auto flex flex-col items-center space-y-6 py-16 lg:py-20">
+          <h2 className="text-4xl lg:text-[3.5rem] font-bold text-start lg:text-center leading-[1.2]">
             Taking Your Business to the Summit of{" "}
             <span className="text-teal-500">Digital Success</span>
           </h2>
-          <p className="max-w-screen-sm text-gray-400 text-start lg:text-center lg:text-lg">
+          <Text className="max-w-screen-sm text-gray-400 text-start lg:text-center">
             We specialize in unleashing the full potential of your business by
             combining the power of creativity, and technical expertise.
-          </p>
+          </Text>
           <SubscribeForm />
         </div>
+
         <Image
           src={HeroImg}
           alt="hero-image"
@@ -29,6 +34,31 @@ function Hero() {
   );
 }
 
-export const Effect = () => <div className={styles.heroEffect}></div>;
+export const Effect = () => (
+  <svg
+    className="w-11/12 max-h-[850px] text-vpurple-900 absolute right-0 bottom-0 hidden lg:block"
+    viewBox="0 0 1101 900"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1440 0H500C223.858 0 0 223.858 0 500V900H1111L1440 0Z"
+      fill="url(#paint0_linear_2245_3)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_2245_3"
+        x1="720"
+        y1="0"
+        x2="720"
+        y2="900"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="currentColor" stop-opacity="0.38" />
+        <stop offset="1" stop-color="currentColor" stop-opacity="0" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
 
 export default Hero;
