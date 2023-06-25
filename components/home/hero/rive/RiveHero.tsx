0e23ebd2628f814b8b02d9dbd17b105d2680872f
Santo Khan <santokhan1999@gmail.com>
2023-06-25 22:17:30 +0600

    update rive

--- components/home/hero/rive/RiveHero.tsx
+++ components/home/hero/rive/RiveHero.tsx
@@ -1,18 +1,29 @@
 // https://cdn.rive.app/animations/vehicles.riv
 
 // @ts-nocheck
-import { useRive } from '@rive-app/react-canvas';
+import { useRive, UseRiveOptions, UseRiveParameters } from '@rive-app/react-canvas';
 
 
 export default function Rive() {
+    const STATE_MACHINE_NAME = "Basic State Machine"
+    const INPUT_Name = "Switch"
+
     const { rive, RiveComponent } = useRive({
         src: "hero.riv",
         autoplay: true,
+        stateMachines: STATE_MACHINE_NAME
     });
+
+    function handleAnimate() {
+        if (rive) {
+            rive.play();
+            console.log(UseRiveOptions);
+        }
+    }
 
     return (
         <div className="flex justify-center">
-            <RiveComponent className='w-full h-[50vw] lg:h-[700px]' onLoad={() => rive && rive.play()} />
+            <RiveComponent className='w-full h-[50vw] lg:h-[700px]' onLoad={handleAnimate} />
         </div>
     );
 }
